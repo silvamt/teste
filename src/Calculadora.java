1352194b02945bfcd7df69bc068a00f629dd49c4
silvamt <marcusts@gmail.com>
2022-07-19 19:04:18 -0300

    atualizar mensagens

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a, b;
 
-        System.out.println("digite a: ");
+        System.out.println("Digite o primeiro valor: ");
         a = scan.nextInt();
-        System.out.println("digite b: ");
+        System.out.println("Digite o segundo valor: ");
         b = scan.nextInt();
 
-        int soma = soma(a, b);
-        System.out.println("soma:" + soma);
-        int subtracao = subtracao(a, b);
-        System.out.println("subtração:" + subtracao);
-        int multiplicacao = multiplicacao(a, b);
-        System.out.println("multiplicação:" + multiplicacao);
-        float divisao = divisao(a, b);
-        System.out.println("divisão:" + divisao);
+        int soma = somar(a, b);
+        System.out.println("Soma:" + soma);
+        int subtracao = subtrair(a, b);
+        System.out.println("Subtração:" + subtracao);
+        int multiplicacao = multiplicar(a, b);
+        System.out.println("Multiplicação:" + multiplicacao);
+        float divisao = dividir(a, b);
+        System.out.println("Divisão:" + divisao);
     }
 
-    public static int soma(int a, int b) {
+    public static int somar(int a, int b) {
         return a + b;
     }
 
-    public static int subtracao(int a, int b) {
+    public static int subtrair(int a, int b) {
         return a - b;
     }
 
-    public static int multiplicacao(int a, int b) {
+    public static int multiplicar(int a, int b) {
         return a * b;
     }
 
-    public static float divisao(int a, int b) {
+    public static float dividir(int a, int b) {
         return a / b;
     }
 }
