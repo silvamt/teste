3227163a747589f7216c0b2e020c52a23cbe2e0c
silvamt <marcusts@gmail.com>
2022-07-19 18:16:36 -0300

    calculadora

--- src/App.java
+++ src/App.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Gato gato = new Gato();
-        System.out.println(gato);
+        Scanner scan = new Scanner(System.in);
+        int a, b;
 
+        System.out.println("digite a: ");
+        a = scan.nextInt();
+        System.out.println("digite b: ");
+        b = scan.nextInt();
+
+        int soma = soma(a, b);
+        System.out.println("soma:" + soma);
+        int subtracao = subtracao(a, b);
+        System.out.println("subtração:" + subtracao);
+        int multiplicacao = multiplicacao(a, b);
+        System.out.println("multiplicação:" + multiplicacao);
+        float divisao = divisao(a, b);
+        System.out.println("divisão:" + divisao);
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicacao(int a, int b) {
+        return a * b;
+    }
+
+    public static float divisao(int a, int b) {
+        return a / b;
     }
 }
