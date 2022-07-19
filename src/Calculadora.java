b7954bbbe6c38a763ab6940fa86c8186e8d8f633
silvamt <marcusts@gmail.com>
2022-07-19 18:27:43 -0300

    renomeando arquivos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a, b;
